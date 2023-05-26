86558d2db2e0bd61b273ed3938d253dac3715cc9
Luis Gonzalez <luisgnzhdz@gmail.com>
2023-05-26 00:26:09 -0400

    style: started with the query interface

--- Codigo_PHP/edadCalculo.php
+++ Codigo_PHP/edadCalculo.php
@@ -4,7 +4,7 @@
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Conexion a la Base de Datos</title>
+  <title>Calculo de edad</title>
 </head>
 <body>
   <?php
